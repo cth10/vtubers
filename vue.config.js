dc3a203a22bedaa3b692d59bc383678041048066
cthamasaki <116269217+cth10@users.noreply.github.com>
2025-05-31 08:17:55 +0000

    Restored to 'bf481706f620613fe848699bafb579e279ff12a9'
    Replit-Restored-To: bf481706f620613fe848699bafb579e279ff12a9

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' // Raiz para domínio personalizado
+    ? '/' // Alterado para a raiz do domínio personalizado
     : '/',
   devServer: {
     host: '0.0.0.0',
